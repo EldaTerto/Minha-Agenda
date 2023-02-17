4b17073f583380f1719235df889f537628c793c4
Elda Ferreira Terto <112215640+EldaTerto@users.noreply.github.com>
2023-02-17 15:43:04 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function enviarDados () {
+    alert("Dados salvo com sucesso")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+        <h1>Minha Agenda</h1>
+    </header>
+    <section class="quad_inicial">
+        <form class="formulario">
+            <label for="nome">Nome</label> 
+            <input id="nome" type="text" placeholder="Nome do contato..." required class="campo_input"></input>
+            <label for="Email">Email</label>
+            <input type="email"  id="Email" placeholder="Email" class="campo_input"></input>
+            <label for="Telefone">Telefone</label>
+            <input type="tel" id="Telefone" placeholder="Telefone" class="campo_input"></input>
+            <label for="Endereço">Endereço</label>
+            <input type="text" id="Endereço" placeholder="Endereço" class="campo_input"></input>
+            <input class="botao" type="submit" value="Salvar" onClick={enviarDados}></input>
+        </form>
+      </section>
     </div>
   );
 }
